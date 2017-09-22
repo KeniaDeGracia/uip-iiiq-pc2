00406ce8708d10f94539646230787f7f84cbb0ca
KeniaDeGracia <keniadegracia96@gmail.com>
2017-09-21 21:09:36 -0600

    Ejemplo 1 de Java

---
+++ Proyecto/src/pa/edu/uip/Main.java
@@ -0,0 +1,29 @@
+package pa.edu.uip;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Bienvenidos al mundo de Java!");// write your code here
+
+        // Tipos de Enteros
+        byte d; //8
+        short b; //16
+        int a = 0; //32
+        long c; //64
+
+        //Tipos de Decimales
+        float e=0.0f; //32
+        double f=0.0; //64
+
+        // Tipos de Caracteres
+        char g = 'a';
+
+        //Cadenas
+        String h = "a";
+
+        //Booleano
+        boolean i = true;
+        i = false;
+        System.out.println(a);
+    }
+}
